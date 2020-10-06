2817824eefb79b2d0b82b1bd4385d5f2ce61360a
Ferrox <ferrox@ferrox.tech>
2020-10-06 19:13:58 +0200

    various improvements and options. Also code commented

--- main.py
+++ main.py
@@ -4,43 +4,78 @@ import imutils
 import cv2
 from classes.BasicSign import BasicSign
 from runners.shapeRunner import search
+from _thread import start_new_thread
+######## Configuration ##########
 
-registeredObjects = [BasicSign(3, "30 KM/h", "assets/thirtySign.jpg",0.20, "sign30")]
+### Basic options
+# Filename for the input source
+videoFile = "assets/fahrt1.mp4"
+# Wether there should be a visualization
+visualise = True
+# use multithreading for the search of an object (Shape search will not be affected by this setting)
+# ToDo: Does not work because QObject::startTimer: Timers cannot be started from another thread
+multithreading = False
+# Crop the input source to a specific area (Parameters for the area in the image options)
+crop = True
+
+### Image options
+# Crop parameters
+xStart = 200
+xStop = 500
+yStart = 100
+yStop = 550
+
+### Register your shapes and objects here
+# ToDo: Add output in shape handling for signs that can be determined only by shape
+# ToDo: Add paramenter to sign definition, if the sign overides speed limit, to avoid a parkverbot sign overriding the 30 sign
+registeredObjects = [BasicSign(4, "Parkverbot", "assets/park.jpg",0.21, "signpark"), BasicSign(4, "30 KM/h", "assets/thirtySign.jpg",0.20, "sign30")]
 registeredShapes = [BasicSign(2, "PLACEHOLDER", "assets/sign2.jpg", 0.25, "shapeRound")]
+
+####### End Configuration #######
+
 
 
 # Load Video File
 # ToDo: Implement switch for video file or live video
-cap = cv2.VideoCapture('assets/fahrt1.mp4')
+cap = cv2.VideoCapture(videoFile)
 
 cv2.waitKey(1)
 frameCount = 0
+# ToDo: Add FPS overlay
+
 
 while(True):
     frameCount+=1
     ret, frame = cap.read()
     image = frame
+    if (crop is True):
+        image=image[yStart:yStop, xStart:xStop]
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(gray, 100, 200)
     found = None
     clone = None
     clone = np.dstack([edged, edged, edged])
 
-    # ToDo: only doing one shape template since there is no other
-    for baseTemplate in registeredShapes[0].templateList:
-        foundShape = search(edged, baseTemplate, registeredShapes[0].message, registeredShapes[0].threshold, False)
 
-        if foundShape is not None:
-            print("Found Shape")
-            for signTemplate in registeredObjects[0].templateList:
-                foundObject = search(edged, signTemplate, registeredObjects[0].message, registeredObjects[0].threshold, True)
-                if foundObject is not None:
-                    print("found object")
-                    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(foundObject)
-                    cv2.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + signTemplate.tW, maxLoc[1] + signTemplate.tH),(0, 0, 255), 2)
-                    cv2.imshow("Visualize", clone)
-                    cv2.waitKey(0)
-            # ToDo Finesearch in multithread
+    for shape in registeredShapes:
+        for baseTemplate in shape.templateList:
+            foundShape = search(edged, baseTemplate, shape.message, shape.threshold, False)
+            if foundShape is not None:
+                for sign in registeredObjects:
+                    for signTemplate in sign.templateList:
+                        if multithreading is True:
+                            start_new_thread(search, (edged, signTemplate, sign.message, sign.threshold, True,))
+                        # If multithreading is on, you cant use visualisation of the found object, since there is no return value atm
+                        if(visualise is True and multithreading is False):
+                            foundObject = search(edged, signTemplate, sign.message, sign.threshold, True)
+                            if foundObject is not None:
+                                (_, maxVal, _, maxLoc) = cv2.minMaxLoc(foundObject)
+                                cv2.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + signTemplate.tW, maxLoc[1] + signTemplate.tH),(0, 0, 255), 2)
+                                cv2.imshow("Visualize", clone)
+                                cv2.waitKey(0)
 
-        cv2.imshow("Visualize", clone)
+        if visualise is True:
+            cv2.imshow("Visualize", clone)
+            cv2.imshow("Image", image)
         cv2.waitKey(1)
